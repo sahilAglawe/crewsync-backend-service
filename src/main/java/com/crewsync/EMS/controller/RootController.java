52fdb478ce6ef5e49022307f9b9d80c9500eb438
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-05 14:27:25 +0530

    first commit

---
+++ src/main/java/com/crewsync/EMS/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.crewsync.EMS.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+	@GetMapping
+    public String helloMsg(){
+        return "Welcome to EMS backend!";
+    }
+}
