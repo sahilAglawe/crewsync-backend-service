0b1bf30f4b552e8d435d2e99d6f0167e8fd618a5
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-06 14:56:17 +0530

    add BatchProgress entity with fields for title, description, document URL, date, and relationships to Batch and Trainer

--- src/main/java/com/crewsync/EMS/entity/BatchProgress.java
+++ src/main/java/com/crewsync/EMS/entity/BatchProgress.java
@@ -1,5 +1,37 @@
 package com.crewsync.EMS.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BatchProgress {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    private String documentUrl;
+
+    private LocalDate date;
+
+    // Select batch from batch table
+    @ManyToOne
+    @JoinColumn(name = "batch_id")
+    private Batch batch;
+
+    // Trainer who added progress
+    @ManyToOne
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
 }
