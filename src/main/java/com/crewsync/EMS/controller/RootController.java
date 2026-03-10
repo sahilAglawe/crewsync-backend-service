f9350830d5db4c997218852cd968ea0641162925
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-10 13:04:07 +0530

    fix: update welcome message in RootController

--- src/main/java/com/crewsync/EMS/controller/RootController.java
+++ src/main/java/com/crewsync/EMS/controller/RootController.java
@@ -8,6 +8,6 @@ public class RootController {
 
 	@GetMapping
     public String helloMsg(){
-        return "Welcome to Crewsync backend!";
+        return "Welcome to Crewsync backend...!";
     }
 }
