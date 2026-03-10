20a1abac8bd2d2cdde7ad57537aec8bc15ef05f0
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-10 12:55:12 +0530

    add AnalystRepository interface extending JpaRepository

---
+++ src/main/java/com/crewsync/EMS/repository/AnalystRepository.java
@@ -0,0 +1,9 @@
+package com.crewsync.EMS.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crewsync.EMS.entity.Analyst;
+
+public interface AnalystRepository extends JpaRepository<Analyst, Long> {
+
+}
