d2f132a2a5dfde384b1610dd9174d1908e7e0f7a
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-11 12:34:21 +0530

    add: create ApiResponse class for standardized API responses

---
+++ src/main/java/com/crewsync/EMS/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.crewsync.EMS.response;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+
+	private boolean status;
+    private String message;
+    private T data;
+}
