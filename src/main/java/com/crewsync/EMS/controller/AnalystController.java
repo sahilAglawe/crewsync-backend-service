10fc6434c8b25313a745d639cef49975ba99b4bc
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-13 11:45:41 +0530

    add: implement AnalystController for managing Analyst entities

---
+++ src/main/java/com/crewsync/EMS/controller/AnalystController.java
@@ -0,0 +1,46 @@
+package com.crewsync.EMS.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crewsync.EMS.dto.AdminDTO;
+import com.crewsync.EMS.dto.AnalystDTO;
+import com.crewsync.EMS.service.AdminService;
+import com.crewsync.EMS.service.AnalystService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/analysts")
+@RequiredArgsConstructor
+public class AnalystController {
+
+	private final AnalystService analystService;
+	
+	@PostMapping
+	public AnalystDTO createAnalyst(@RequestBody AnalystDTO analystDTO) {
+		return analystService.createAnalyst(analystDTO);
+	}
+	
+	@GetMapping
+    public List<AnalystDTO> getAllAnalysts() {
+        return analystService.getAllAnalysts();
+    }
+
+	 @GetMapping("/{id}")
+	    public AnalystDTO getAnalystById(@PathVariable Long id) {
+	        return analystService.getAnalystById(id);
+	    }
+	 
+	 @DeleteMapping("/{id}")
+	    public void deleteAnalyst(@PathVariable Long id) {
+	        analystService.deleteAnalyst(id);
+	    }
+}
