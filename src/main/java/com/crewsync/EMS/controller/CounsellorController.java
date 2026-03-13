ac2d12caae4b8b4f4bf7156433a99b9e1daa587e
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-13 12:15:16 +0530

    add: implement CounsellorController and its service for managing Counsellor entities

---
+++ src/main/java/com/crewsync/EMS/controller/CounsellorController.java
@@ -0,0 +1,46 @@
+package com.crewsync.EMS.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crewsync.EMS.dto.AnalystDTO;
+import com.crewsync.EMS.dto.CounsellorDTO;
+import com.crewsync.EMS.service.AnalystService;
+import com.crewsync.EMS.service.CounsellorService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/counsellors")
+@RequiredArgsConstructor
+public class CounsellorController {
+
+	private final CounsellorService counsellorService;
+	
+	@PostMapping
+	public CounsellorDTO createCounsellor(@RequestBody CounsellorDTO counsellorDTO) {
+		return counsellorService.createCounsellor(counsellorDTO);
+	}
+	
+	@GetMapping
+    public List<CounsellorDTO> getAllCounsellors() {
+        return counsellorService.getAllCounsellors();
+    }
+	
+	@GetMapping("/{id}")
+	public CounsellorDTO getCounsellorById(@PathVariable Long id) {
+		return counsellorService.getCounsellorById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteCounsellor(@PathVariable Long id) {
+	  counsellorService.deleteCounsellor(id);
+	}
+}
