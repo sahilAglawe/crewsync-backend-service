d764881c0372c6e904e5907a0d968426b24e02bf
SahilAglawe <sahilaglawe66@gmail.com>
2026-03-14 16:16:01 +0530

    Counellor Controller

--- src/main/java/com/crewsync/EMS/controller/CounsellorController.java
+++ src/main/java/com/crewsync/EMS/controller/CounsellorController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import com.crewsync.EMS.dto.CounsellorDTO;
-
-import com.crewsync.EMS.service.CounsellorService;
+import com.crewsync.EMS.dto.StudentDTO;
+import com.crewsync.EMS.service.StudentService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,25 +20,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CounsellorController {
 
-	private final CounsellorService counsellorService;
-	
-	@PostMapping
-	public CounsellorDTO createCounsellor(@RequestBody CounsellorDTO counsellorDTO) {
-		return counsellorService.createCounsellor(counsellorDTO);
-	}
-	
-	@GetMapping
-    public List<CounsellorDTO> getAllCounsellors() {
-        return counsellorService.getAllCounsellors();
-    }
-	
-	@GetMapping("/{id}")
-	public CounsellorDTO getCounsellorById(@PathVariable Long id) {
-		return counsellorService.getCounsellorById(id);
-	}
-	
-	@DeleteMapping("/{id}")
-	public void deleteCounsellor(@PathVariable Long id) {
-	  counsellorService.deleteCounsellor(id);
-	}
+
+	 private final StudentService studentService;
+	 
+	 @PostMapping("/students")
+	    public StudentDTO addStudent(@RequestBody StudentDTO studentDTO) {
+	        return studentService.createStudent(studentDTO);
+	    }
+
+	    @GetMapping("/students")
+	    public List<StudentDTO> getAllStudents() {
+	        return studentService.getAllStudents();
+	    }
+
+	    @GetMapping("/students/{id}")
+	    public StudentDTO getStudent(@PathVariable Long id) {
+	        return studentService.getStudentById(id);
+	    }
+
+	    @DeleteMapping("/students/{id}")
+	    public String deleteStudent(@PathVariable Long id) {
+	        studentService.deleteStudent(id);
+	        return "Student deleted successfully";
+	    }
 }
