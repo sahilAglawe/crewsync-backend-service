45691809d2a9de5af2da03f6affdfc93a5e63879
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-03-26 11:27:51 +0530

    Update RootController.java

--- src/main/java/com/crewsync/EMS/controller/RootController.java
+++ src/main/java/com/crewsync/EMS/controller/RootController.java
@@ -3,6 +3,7 @@ package com.crewsync.EMS.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class RootController {
 
